51a62d1fdfa1345a1b133a04dc73a96de1ee0da8
Shutko Svitlana <svetalysenko25@gmail.com>
2024-02-07 14:26:22 +0200

    added homework 13 Tomcat

--- src/main/java/homework13/HelloServlet.java
+++ src/main/java/homework13/HelloServlet.java
@@ -7,6 +7,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
+
 @WebServlet(name = "HelloServlet", urlPatterns = {"/hello"})
 public class HelloServlet extends HttpServlet {
     @Override
